e29f70a5f521f1725d3324227e0a0478451ad74b
Aditya Baddur <adityabaddur@Adityas-MacBook-Air.local>
2022-12-16 10:31:20 +0530

    header as custom element

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,32 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, Injector, NgModule, Type } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  bootstrap: [AppComponent],
+  bootstrap: [],
 })
-export class AppModule {}
+export class AppModule implements DoBootstrap {
+
+  constructor(private injector: Injector) {
+    
+  }
+
+  creteCustomElement(component: Type<any>, selectorName: string): any {
+    const customElement = createCustomElement(component, { injector: this.injector });
+    customElements.define(selectorName, customElement);
+  }
+
+  ngDoBootstrap(appRef: ApplicationRef): void {
+    this.creteCustomElement(AppHeaderComponent, 'app-header-ce')
+  }
+
+}
