9ad9fd05d593f562b4865f13b6826f9086986c25
Aditya Baddur <adityabaddur@gmail.com>
2022-12-16 13:07:00 +0530

    remove comment html

--- angularjs/app1/users/users.controller.js
+++ angularjs/app1/users/users.controller.js
@@ -7,15 +7,6 @@
 
   function UsersCtrl($stateParams, UsersService) {
     const vm = this;
-
-    const init = function() {
-      vm.email = $stateParams.email;
-      UsersService.getUsers().then(resp => {
-        vm.users = resp.data.data;
-      });
-    };
-
-    init();
   }
 
 })();
