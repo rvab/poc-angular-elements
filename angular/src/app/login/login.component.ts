de710d30133db3b77b77fad31dba1ea1c811b3de
Aditya Baddur <adityabaddur@Adityas-MacBook-Air.local>
2022-12-16 12:28:27 +0530

    created angular services

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { catchError, from, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent {
   loginForm: FormGroup<{email: FormControl, password: FormControl}>;
   loginFailed = false;
 
-  constructor(@Inject('AuthService') private authService: any) {
+  constructor(private authService: AuthService) {
     this.loginForm = new FormGroup<{email: FormControl, password: FormControl}>({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
@@ -19,7 +20,7 @@ export class LoginComponent {
   }
 
   login() {
-    from(this.authService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value)).pipe(
+    this.authService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value).pipe(
       catchError((error) => {
         this.loginFailed = true;
         return throwError(() => error);
